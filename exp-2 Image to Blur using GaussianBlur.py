126147c1b10b4f881b197f1d3d81b33e66246de6
saijagadeesh-88 <192472146.simats@saveetha.com>
2026-07-21 14:45:04 +0530

    Delete exp-2 Image to Blur using GaussianBlur.py

--- exp-2 Image to Blur using GaussianBlur.py
+++
@@ -1,7 +0,0 @@
-import cv2 
-image = cv2.imread("image.png")  
-blurred_image = cv2.GaussianBlur(image, (15, 15), 0)   
-cv2.imshow("Original Image", image) 
-cv2.imshow("Blurred Image", blurred_image) 
-cv2.waitKey(0) 
-cv2.destroyAllWindows() 
