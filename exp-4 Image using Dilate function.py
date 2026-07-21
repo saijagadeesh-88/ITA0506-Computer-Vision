d18d407d7c2ec5f08244c91beab29bbf26d87887
saijagadeesh-88 <192472146.simats@saveetha.com>
2026-07-21 14:44:24 +0530

    Delete exp-4 Image using Dilate function.py

--- exp-4 Image using Dilate function.py
+++
@@ -1,8 +0,0 @@
-import cv2 
-image = cv2.imread("image.png")  
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
-equalized_image = cv2.equalizeHist(gray_image) 
-cv2.imshow("Original Grayscale Image", gray_image) 
-cv2.imshow("Histogram Equalized Image", equalized_image) 
-cv2.waitKey(0) 
-cv2.destroyAllWindows()
